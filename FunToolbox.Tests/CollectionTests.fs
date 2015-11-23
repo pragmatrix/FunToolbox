c6c7dc43ff1eb8335f0c952b6973db4729a5236a
Armin Sander <armin@replicator.org>
2015-11-23 15:11:10 +0100

    first commit: SortedArray.slice

---
+++ FunToolbox.Tests/CollectionTests.fs
@@ -0,0 +1,119 @@
+﻿namespace FunToolbox.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open FunToolbox
+
+[<TestFixture>]
+type CollectionTests() =
+
+    [<Test>]
+    member this.sortedArrayMatchingTests() = 
+        
+        // end range, matching
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (1,4)
+        |> should equal [1;2;3;4]
+
+        // begin range, matching
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (0,1)
+        |> should equal [0;1]
+
+        // inner range, matching
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (1,3)
+        |> should equal [1;2;3]
+
+        // too low, matching
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (-1,2)
+        |> should equal [0;1;2]
+        
+        // too high, matching
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (3,5)
+        |> should equal [3;4]
+
+        // one element, matching
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (2,2)
+        |> should equal [2]
+
+        // first element, matching
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (0,0)
+        |> should equal [0]
+
+        // last element, matching
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (4,4)
+        |> should equal [4]
+
+    [<Test>]
+    member this.sortedNonMatchingTests() = 
+        
+        // end range
+        [|0;2;4;6;8|]
+        |> SortedArray.slice (5,9)
+        |> should equal [6;8]
+
+        // begin range
+        [|0;2;4;6;8|]
+        |> SortedArray.slice (-1,3)
+        |> should equal [0;2]
+
+        // inner range
+        [|0;2;4;6;8|]
+        |> SortedArray.slice (3,7)
+        |> should equal [4;6]
+
+        // too low
+        [|0;2;4;6;8|]
+        |> SortedArray.slice (-1,3)
+        |> should equal [0;2]
+        
+        // too high
+        [|0;2;4;6;8|]
+        |> SortedArray.slice (5,9)
+        |> should equal [6;8]
+
+        // one element
+        [|0;2;4;6;8|]
+        |> SortedArray.slice (1,3)
+        |> should equal [2]
+
+        // first element
+        [|0;2;4;6;8|]
+        |> SortedArray.slice (0,1)
+        |> should equal [0]
+
+        // last element
+        [|0;2;4;6;8|]
+        |> SortedArray.slice (7,8)
+        |> should equal [8]
+
+    [<Test>]
+    member this.sortedArrayOutOfRangeTests() = 
+
+        // range completely too low
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (-2,-1)
+        |> should equal []
+
+        // range completely too high
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (5,6)
+        |> should equal []
+
+        // invalid range
+        [|0;1;2;3;4|]
+        |> SortedArray.slice (2,1)
+        |> should equal []
+
+        [||]
+        |> SortedArray.slice(0,1)
+        |> should equal []
+
+        
---
+++ FunToolbox/Collections.fs
@@ -0,0 +1,18 @@
+﻿namespace FunToolbox
+
+open System
+
+module SortedArray = 
+    
+    /// Returns the sorted sequence ragne of values from low to high (including)
+    let slice  (low: 'v, high: 'v) (sa: 'v array) : 'v seq =
+        let first = Array.BinarySearch(sa, low)
+        let first = if first < 0 then ~~~first else first
+        if first = sa.Length then Seq.empty
+        else
+        let last = Array.BinarySearch(sa, high)
+        if last >= 0 then
+            seq {first..last}
+        else
+            seq {first..(~~~last)-1}
+        |> Seq.map (fun i -> sa.[i])
