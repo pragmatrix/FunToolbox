f6a5ddf4035906d9ec923f50278b0114c32924dc
Armin Sander <armin@replicator.org>
2015-11-23 15:37:31 +0100

    slice -> range, sophisticate project structure

--- FunToolbox/Collections.fs
+++ FunToolbox/Collections.fs
@@ -5,7 +5,7 @@ open System
 module SortedArray = 
     
     /// Returns the sorted sequence ragne of values from low to high (including)
-    let slice  (low: 'v, high: 'v) (sa: 'v array) : 'v seq =
+    let range  (low: 'v, high: 'v) (sa: 'v array) : 'v seq =
         let first = Array.BinarySearch(sa, low)
         let first = if first < 0 then ~~~first else first
         if first = sa.Length then Seq.empty
@@ -14,5 +14,9 @@ module SortedArray =
         if last >= 0 then
             seq {first..last}
         else
-            seq {first..(~~~last)-1}
+            let last = ~~~last
+            // last points eiter (a) the first one that is > high or (b) equals the size of the array
+            // for (a), we subtract 1 so that we don't the > element
+            // for (b), we subtract 1 to include the last element.
+            seq {first..last-1}
         |> Seq.map (fun i -> sa.[i])
