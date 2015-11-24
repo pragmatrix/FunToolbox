926f36d7c05ede1b5c5f44aca0f9ca32656b9a2e
Armin Sander <armin@replicator.org>
2015-11-24 11:08:11 +0100

    put SortedArray into SortedArray.fs, encapsulate it, and add some more functions

--- FunToolbox.Tests/SortedArrayTests.fs
+++ FunToolbox.Tests/SortedArrayTests.fs
@@ -6,114 +6,151 @@ open FsUnit
 open FunToolbox
 
 [<TestFixture>]
-type CollectionTests() =
+type SortedArrayTests() =
 
     [<Test>]
     member this.sortedArrayRangeMatchingTests() = 
+        let array = 
+            [0;1;2;3;4]
+            |> SortedArray.ofSeq
         
         // end range, matching
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (1,4)
         |> should equal [1;2;3;4]
 
         // begin range, matching
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (0,1)
         |> should equal [0;1]
 
         // inner range, matching
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (1,3)
         |> should equal [1;2;3]
 
         // too low, matching
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (-1,2)
         |> should equal [0;1;2]
         
         // too high, matching
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (3,5)
         |> should equal [3;4]
 
         // one element, matching
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (2,2)
         |> should equal [2]
 
         // first element, matching
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (0,0)
         |> should equal [0]
 
         // last element, matching
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (4,4)
         |> should equal [4]
 
     [<Test>]
     member this.sortedArrayRangeNonMatchingTests() = 
         
+        let array = 
+            [0;2;4;6;8] 
+            |> SortedArray.ofSeq
+
         // end range
-        [|0;2;4;6;8|]
+        array
         |> SortedArray.range (5,9)
         |> should equal [6;8]
 
         // begin range
-        [|0;2;4;6;8|]
+        array
         |> SortedArray.range (-1,3)
         |> should equal [0;2]
 
         // inner range
-        [|0;2;4;6;8|]
+        array
         |> SortedArray.range (3,7)
         |> should equal [4;6]
 
         // too low
-        [|0;2;4;6;8|]
+        array
         |> SortedArray.range (-1,3)
         |> should equal [0;2]
         
         // too high
-        [|0;2;4;6;8|]
+        array
         |> SortedArray.range (5,9)
         |> should equal [6;8]
 
         // one element
-        [|0;2;4;6;8|]
+        array
         |> SortedArray.range (1,3)
         |> should equal [2]
 
         // first element
-        [|0;2;4;6;8|]
+        array
         |> SortedArray.range (0,1)
         |> should equal [0]
 
         // last element
-        [|0;2;4;6;8|]
+        array
         |> SortedArray.range (7,8)
         |> should equal [8]
 
     [<Test>]
     member this.sortedArrayRangeOutOfRangeTests() = 
 
+        let array = 
+            [0;1;2;3;4]
+            |> SortedArray.ofSeq
+
         // range completely too low
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (-2,-1)
         |> should equal []
 
         // range completely too high
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (5,6)
         |> should equal []
 
         // invalid range
-        [|0;1;2;3;4|]
+        array
         |> SortedArray.range (2,1)
         |> should equal []
 
-        [||]
+        [] 
+        |> SortedArray.ofSeq
         |> SortedArray.range(0,1)
         |> should equal []
 
-        
+    [<Test>]
+    member this.sortedArrayContains() = 
+        let array = 
+            [0;2]
+            |> SortedArray.ofSeq
+
+        array
+        |> SortedArray.contains 3
+        |> should equal false
+
+        array
+        |> SortedArray.contains 1
+        |> should equal false
+
+        array
+        |> SortedArray.contains 0
+        |> should equal true
+
+        array
+        |> SortedArray.contains -1
+        |> should equal false
+
+        array
+        |> SortedArray.contains 2
+        |> should equal true
+
---
+++ FunToolbox/SortedArray.fs
@@ -0,0 +1,58 @@
+﻿namespace FunToolbox
+
+type 'v sarray = SortedArray of 'v array
+
+module SortedArray =
+    
+    open System
+
+    /// Converts a sequence to a SortedArray
+    let ofSeq s = 
+        s
+        |> Seq.toArray
+        |> Array.sort
+        |> SortedArray
+
+    /// Converts a SortedArray to an array
+    let toArray (sa: 'v sarray) = 
+        let (SortedArray a) = sa
+        a
+
+    /// Converts a SortedArray to an list
+    let toList (sa: 'v sarray) = 
+        sa |> toArray |> Array.toList
+
+    /// Converts a SortedArray to a sequence
+    let toSeq (sa: 'v sarray) = 
+        sa |> toArray |> Array.toSeq
+
+    /// Returns the lower bound (the index of the value, or the next larger one, or the length of the
+    /// array if none found)
+    let lowerBound low (sa: 'v sarray) = 
+        let first = Array.BinarySearch(sa |> toArray, low)
+        if first < 0 then ~~~first else first
+
+    /// Returns the upper bound (the index of the value + 1, or the next larger one, or the length of the
+    /// array if none found)
+    let upperBound high (sa: 'v sarray) = 
+        let last = Array.BinarySearch(sa |> toArray, high)
+        if last >= 0 then last + 1 else ~~~last
+
+    /// Returns the sorted sequence ragne of values from low to high (including)
+    let range (low: 'v, high: 'v) (sa: 'v sarray) : 'v seq =
+        let lower = lowerBound low sa
+        let upper = upperBound high sa
+        let a = sa |> toArray
+        seq {lower..upper-1}
+        |> Seq.map (fun i -> a.[i])
+
+    /// Returns Some index if the value is in the array, otherwise None
+    let indexOf (value: 'v) (sa: 'v sarray) : int option = 
+        let r = Array.BinarySearch(sa |> toArray, value)
+        if r >= 0 then Some r
+        else None
+
+    /// Returns true if the value is in the array, otherwise false
+    let contains (value: 'v) (sa: 'v sarray) : bool = 
+        Array.BinarySearch(sa |> toArray, value) >= 0
+
