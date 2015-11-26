12ec8bc1faa6a9cdd7b7e21a322ae891d3495062
Armin Sander <armin@replicator.org>
2015-11-26 17:13:40 +0100

    introduce simple, atomic variables

---
+++ FunToolbox/Atom.fs
@@ -0,0 +1,18 @@
+﻿namespace FunToolbox
+
+type 'v atom = private {
+    mutable value: 'v
+}
+
+module Atom =
+    let create v = { value = v }
+    let swap (f: 'v -> 'v) (a: 'v atom) : 'v = 
+        lock a (fun () -> a.value <- f a.value; a.value)
+    let value a = 
+        lock a (fun () -> a.value)
+    let set v a = 
+        a
+        |> swap (fun _ -> v)
+        |> ignore
+
+
