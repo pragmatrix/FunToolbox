c07966cd9f464c87a782dacd95e6e62ee1c4e9ac
Armin Sander <armin@replicator.org>
2015-12-09 10:30:06 +0100

    lowercase String.startsWith / endsWith ordinal comparison

--- FunToolbox/Prelude.fs
+++ FunToolbox/Prelude.fs
@@ -2,6 +2,8 @@
 
 module Prelude =
 
+    open System
+
     [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
     module Option =
 
@@ -13,6 +15,13 @@ module Prelude =
 
     /// Equivalent to the <| operator, but with a more useful priority to separate funs, etc.
     let inline (--) a b = a b
+
+    // ordinal startsWith / endsWith for strings
+    type System.String with
+        member this.startsWith part = 
+            this.StartsWith(part, StringComparison.Ordinal)
+        member this.endsWith part = 
+            this.EndsWith(part, StringComparison.Ordinal)
 
 [<assembly:AutoOpen("FunToolbox.Prelude")>]
 do
