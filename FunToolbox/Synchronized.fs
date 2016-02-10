5e72584ad4f5da589cdcf184c14ccc130c0e5b2b
Armin Sander <armin@replicator.org>
2016-02-10 17:22:14 +0100

    add Synchronized Queue

---
+++ FunToolbox/Synchronized.fs
@@ -0,0 +1,29 @@
+﻿namespace FunToolbox
+
+open System.Threading
+
+module Synchronized =
+
+    // ConcurrentQueue does not quite support what we want here.
+    // duplicated from the IVR project (Threading.SynchronizedQueue<_>)
+    
+    type Queue<'t>() = 
+        let monitor = obj()
+        let queue = System.Collections.Generic.Queue<'t>()
+        
+        member this.enqueue v = 
+            lock monitor (
+                fun () -> 
+                    queue.Enqueue v
+                    if queue.Count = 1 then
+                        Monitor.Pulse monitor
+                )
+
+        member this.dequeue() =
+            lock monitor (
+                fun () ->
+                    while queue.Count = 0 do
+                        Monitor.Wait monitor |> ignore
+
+                    queue.Dequeue()
+                )
