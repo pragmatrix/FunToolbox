f7dc3b861c5707b86280f2f3a2b48c9c0b9a69a0
Armin Sander <armin@replicator.org>
2016-07-02 14:51:26 +0200

    encapsulate the graph so that we can remove the comparison constraint later on

--- FunToolbox/Algorithms.fs
+++ FunToolbox/Algorithms.fs
@@ -1,5 +1,6 @@
 ﻿namespace FunToolbox.Algorithms
 
+open System.Linq
 open FunToolbox.Prelude
 open System.Collections.Generic
 
@@ -8,29 +9,40 @@ module DAG =
 
     exception CycleFoundException
 
-    let sortTopologically (graph: Map<'t, 't list>) = 
+    type Graph<'t> = private Graph of ('t * 't list) list
+    type 't graph = Graph<'t>
 
+    let sortTopologically (Graph graph) = 
+
+        let lookup = graph.ToDictionary((fun (k, _) -> k), (fun (_, v) -> v))
         let visited = HashSet<'t>()
 
         let dfs result start_node = 
             let rec explore path result node = 
-                if Set.contains node path then raise CycleFoundException else
                 if visited.Contains node then result else     
+                if Set.contains node path then raise CycleFoundException else
                     let newPath = Set.add node path
-                    let edges = graph.TryFind node |> Option.orElse (fun () -> [])
+                    let edges = 
+                        match lookup.TryGetValue node with
+                        | true, edges -> edges
+                        | false, _ -> []
+
                     let result  = List.fold (explore newPath) result edges
                     visited.Add node |> ignore
                     node :: result
             in explore Set.empty result start_node
       
-        Seq.fold (fun result (node,_) -> dfs result node) [] (graph |> Map.toList)
+        Seq.fold (fun result (node,_) -> dfs result node) [] graph
 
     let ofEdges (edges: ('t * 't) list) =
         edges
         |> List.groupBy fst
         |> List.map (fun (k, v) -> k, v |> List.map snd)
-        |> Map.ofList
+        |> Graph
 
     let ofList (l: ('t * 't list) list) = 
-        l |> Map.ofList
+        l 
+        |> List.groupBy fst
+        |> List.map (fun (k, v) -> k, v |> List.collect snd)
+        |> Graph
 
