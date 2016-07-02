b288817b300eafa66f2822ac35f9c29f2102b267
Armin Sander <armin@replicator.org>
2016-07-02 15:01:49 +0200

    topological sort: linearize performance and remove the comparison constraint

--- FunToolbox/Algorithms.fs
+++ FunToolbox/Algorithms.fs
@@ -14,23 +14,30 @@ module DAG =
 
     let sortTopologically (Graph graph) = 
 
-        let lookup = graph.ToDictionary((fun (k, _) -> k), (fun (_, v) -> v))
+        // use a dictionary instead of a map, so that we don't need the comparison constraint, and
+        // stay linear in performance.
+        let lookup = graph.ToDictionary(fst, snd)
         let visited = HashSet<'t>()
 
         let dfs result start_node = 
-            let rec explore path result node = 
+
+            // there is only one path when we run a depth first search, 
+            // so we can optimize this by using an immutable HashSet, instead of Set or a List. 
+            // For the set, this also this avoids the comparison constraint
+            let path = HashSet<'t>()
+
+            let rec explore result node = 
                 if visited.Contains node then result else     
-                if Set.contains node path then raise CycleFoundException else
-                    let newPath = Set.add node path
+                if path.Contains node then raise CycleFoundException else
+                    path.Add node |> ignore
                     let edges = 
                         match lookup.TryGetValue node with
                         | true, edges -> edges
                         | false, _ -> []
-
-                    let result  = List.fold (explore newPath) result edges
+                    let result = List.fold explore result edges
                     visited.Add node |> ignore
                     node :: result
-            in explore Set.empty result start_node
+            in explore result start_node
       
         Seq.fold (fun result (node,_) -> dfs result node) [] graph
 
