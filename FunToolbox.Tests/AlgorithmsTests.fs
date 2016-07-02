60b6afa2009586e17210b0c82b97c36fdded0060
Armin Sander <armin@replicator.org>
2016-07-02 14:21:24 +0200

    topological sort, first version

---
+++ FunToolbox.Tests/AlgorithmsTests.fs
@@ -0,0 +1,65 @@
+﻿namespace FunToolbox.Tests
+
+open NUnit.Framework
+open FsUnit
+open FunToolbox.Algorithms
+
+type A = 
+    | A
+    | B 
+    | C
+
+type Recipe = Eggs | Milk | Wheat | Mix | Cook | Serve
+
+
+[<TestFixture>]
+type AlgorithmsTests() = 
+
+    let sortEdges = DAG.ofEdges >> DAG.sortTopologically
+    let graph = [ 
+            Wheat, [Eggs;Milk;Mix] ;
+            Milk,  [Mix] ;
+            Cook,  [Serve] ;
+            Eggs,  [Mix] ;
+            Mix,   [Cook] ;
+            Serve, [] ]
+    
+    [<Test>]
+    member this.SortsTopologically() = 
+        [(B, A); (C, A); (C, B)]
+        |> sortEdges
+        |> should equal [C; B; A]
+
+    [<Test>]
+    member this.SortsTopologically2() = 
+        [(B, A); (C, A); (B, C)]
+        |> sortEdges
+        |> should equal [B; C; A]
+
+    [<Test>]
+    member this.SortsTopologically3() = 
+        graph
+        |> DAG.ofList
+        |> DAG.sortTopologically
+        |> should equal [Wheat; Milk; Eggs; Mix; Cook; Serve]
+
+    [<Test;ExpectedException(typeof<DAG.CycleFoundException>)>]
+    member this.DetectsCycles() = 
+        [(A, B); (C, A); (B, A)]
+        |> sortEdges
+
+    [<Test;ExpectedException(typeof<DAG.CycleFoundException>)>]
+    member this.SelfCycle() = 
+        [(A, A)]
+        |> sortEdges
+
+    [<Test>]
+    member this.Empty() = 
+        []
+        |> sortEdges
+        |> should equal []
+
+
+
+        
+            
---
+++ FunToolbox/Algorithms.fs
@@ -0,0 +1,32 @@
+﻿namespace FunToolbox.Algorithms
+
+open FunToolbox.Prelude
+
+/// Directed, acyclic graphs
+module DAG = 
+
+    exception CycleFoundException
+
+    let sortTopologically (graph: Map<'t, 't list>) = 
+
+        let dfs visited start_node = 
+            let rec explore path visited node = 
+                if Set.contains node path then raise CycleFoundException else
+                if List.contains node visited then visited else     
+                    let new_path = Set.add node path
+                    let edges    = graph.TryFind node |> Option.orElse (fun () -> [])
+                    let visited  = List.fold (explore new_path) visited edges
+                    node :: visited
+            in explore Set.empty visited start_node
+      
+        Seq.fold (fun visited (node,_) -> dfs visited node) [] (graph |> Map.toList)
+
+    let ofEdges (edges: ('t * 't) list) =
+        edges
+        |> List.groupBy fst
+        |> List.map (fun (k, v) -> k, v |> List.map snd)
+        |> Map.ofList
+
+    let ofList (l: ('t * 't list) list) = 
+        l |> Map.ofList
+
