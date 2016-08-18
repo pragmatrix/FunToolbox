c033facfb7a78cee617dc464f441b8a05ed88719
Armin Sander <armin@replicator.org>
2016-08-18 10:59:57 +0200

    reduce indent of declared modules whenever possible

--- FunToolbox/AtomFile.fs
+++ FunToolbox/AtomFile.fs
@@ -1,114 +1,99 @@
-﻿namespace FunToolbox
+﻿module FunToolbox.AtomFile
 
 open System.IO
+open System.Text
+open System.Threading
 
-module AtomFile = 
-    open System.Text
-    open System.Threading
+module private Header = 
+    // we need a header, so we can discriminate between a new file and an empty one.
+    let data = "atomfile\n" |> Seq.map (fun c -> c |> byte) |> Seq.toArray
+    let len = data.Length
+    let is (test: byte[]) = 
+        test.Length = len && 
+        Array.compareWith (fun (l: byte) (r: byte) -> l.CompareTo(r)) data test = 0
 
-    module private Header = 
-        // we need a header, so we can discriminate between a new file and an empty one.
-        let data = "atomfile\n" |> Seq.map (fun c -> c |> byte) |> Seq.toArray
-        let len = data.Length
-        let is (test: byte[]) = 
-            test.Length = len && 
-            Array.compareWith (fun (l: byte) (r: byte) -> l.CompareTo(r)) data test = 0
+[<AutoOpen>]
+module private Helper =
+    let rec openFile fn = 
+        let ts = 
+            try
+                File.Open(fn, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None)
+            with 
+                // be very specific about the error we tolerate here!
+                | :? IOException as e ->
+                if e.HResult = 0x80070020 then null
+                else reraise()
+        // we can not tail-recurse in exception handlers, so we do that here
+        match ts with 
+        | null -> 
+            Thread.Sleep(1)
+            openFile fn
+        | ts -> ts
 
-    [<AutoOpen>]
-    module private Helper =
-        let rec openFile fn = 
-            let ts = 
-                try
-                    File.Open(fn, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None)
-                with 
-                    // be very specific about the error we tolerate here!
-                    | :? IOException as e ->
-                    if e.HResult = 0x80070020 then null
-                    else reraise()
-            // we can not tail-recurse in exception handlers, so we do that here
-            match ts with 
-            | null -> 
-                Thread.Sleep(1)
-                openFile fn
-            | ts -> ts
+    let read (bytes: int) (fs: FileStream) : byte[] = 
+        let a = Array.zeroCreate bytes
+        if bytes <> fs.Read(a, 0, bytes) then
+            failwith "failed to read from file"
+        a
 
-        let read (bytes: int) (fs: FileStream) : byte[] = 
-            let a = Array.zeroCreate bytes
-            if bytes <> fs.Read(a, 0, bytes) then
-                failwith "failed to read from file"
-            a
+    let write (bytes: byte[]) (fs: FileStream) =
+        fs.Write(bytes, 0, bytes.Length)
 
-        let write (bytes: byte[]) (fs: FileStream) =
-            fs.Write(bytes, 0, bytes.Length)
+    let truncate (fs: FileStream) = 
+        fs.SetLength(0 |> int64)
 
-        let truncate (fs: FileStream) = 
-            fs.SetLength(0 |> int64)
+    let getData (fs: FileStream) =
+        let l = fs.Length
+        let li = l |> int
+        if li = 0 then None
+        else
+        let header = fs |> read Header.len
+        if Header.is header |> not then
+            failwith "failed to read header"
+        fs |> read (li - Header.len) |> Some
 
-        let getData (fs: FileStream) =
-            let l = fs.Length
-            let li = l |> int
-            if li = 0 then None
-            else
-            let header = fs |> read Header.len
-            if Header.is header |> not then
-                failwith "failed to read header"
-            fs |> read (li - Header.len) |> Some
+    let setData (fs: FileStream) (data: byte[] option) =
+        fs |> truncate
+        if data.IsSome then
+            try
+                fs |> write Header.data
+                fs |> write data.Value
+            with _ ->
+                // be sure file is empty when writing fails.
+                fs |> truncate
+                reraise()
 
-        let setData (fs: FileStream) (data: byte[] option) =
-            fs |> truncate
-            if data.IsSome then
-                try
-                    fs |> write Header.data
-                    fs |> write data.Value
-                with _ ->
-                    // be sure file is empty when writing fails.
-                    fs |> truncate
-                    reraise()
+/// Writes contents to an atomic file. Creates it, if it's not existing.
+let write fn data = 
+    use fs = openFile fn
+    setData fs data
 
-    /// Writes contents to an atomic file. Creates it, if it's not existing.
-    let write fn data = 
-        use fs = openFile fn
-        setData fs data
+/// Reads the contents of a atomic file.
+let read fn = 
+    use fs = openFile fn
+    getData fs
 
-    /// Reads the contents of a atomic file.
-    let read fn = 
-        use fs = openFile fn
-        getData fs
+type SwapFunction = byte[] option -> byte[] option
 
-    type SwapFunction = byte[] option -> byte[] option
-
-    /// Atomically exchanges the contents of a file.         
-    let swap (f: SwapFunction) fn = 
-        use fs = openFile fn
-        let input = getData fs
+/// Atomically exchanges the contents of a file.         
+let swap (f: SwapFunction) fn = 
+    use fs = openFile fn
+    let input = getData fs
         
-        let output = f input
-        output |> setData fs
-        output
+    let output = f input
+    output |> setData fs
+    output
 
-    module Decode =
-        let toString (e: Encoding) (value: byte[] option)  =
-            value |> Option.map (fun b -> e.GetString(b))
+module Decode =
+    let toString (e: Encoding) (value: byte[] option)  =
+        value |> Option.map (fun b -> e.GetString(b))
 
-    module Encode =
-        let fromString (e: Encoding) (value: string option) = 
-            value |> Option.map (fun str -> e.GetBytes(str))
+module Encode =
+    let fromString (e: Encoding) (value: string option) = 
+        value |> Option.map (fun str -> e.GetBytes(str))
 
-    module Swapper = 
+module Swapper = 
 
-        /// Convert a string swap function to a byte array swap function.
-        let string (e: Encoding) (f: string option -> string option) : (byte[] option -> byte[] option) = 
-            Decode.toString e >> f >> Encode.fromString e
-                
-
-        
-
-        
-
-                
-   
-
-
-
-    
-
+    /// Convert a string swap function to a byte array swap function.
+    let string (e: Encoding) (f: string option -> string option) : (byte[] option -> byte[] option) = 
+        Decode.toString e >> f >> Encode.fromString e
--- FunToolbox/Prelude.fs
+++ FunToolbox/Prelude.fs
@@ -1,121 +1,119 @@
-﻿namespace FunToolbox
+﻿module FunToolbox.Prelude
 
 open System
 open System.Threading.Tasks
 
-module Prelude =
+type CRAttribute = CompilationRepresentationAttribute
 
-    type CRAttribute = CompilationRepresentationAttribute
+[<Literal>]
+let ModuleSuffix = CompilationRepresentationFlags.ModuleSuffix
 
-    [<Literal>]
-    let ModuleSuffix = CompilationRepresentationFlags.ModuleSuffix
+[<CR(ModuleSuffix)>]
+module Option =
 
-    [<CR(ModuleSuffix)>]
-    module Option =
-
-        /// Return the value of the option, call elseValue when it is None.
-        let inline orElse elseValueF option = 
-            match option with
-            | Some v -> v
-            | None -> elseValueF()
+    /// Return the value of the option, call elseValue when it is None.
+    let inline orElse elseValueF option = 
+        match option with
+        | Some v -> v
+        | None -> elseValueF()
         
-        /// Return the value of the option, or elseValue when it is None.
-        let inline orElseConst elseValue option = 
-            match option with
-            | Some v -> v
-            | None -> elseValue
+    /// Return the value of the option, or elseValue when it is None.
+    let inline orElseConst elseValue option = 
+        match option with
+        | Some v -> v
+        | None -> elseValue
 
-        /// Returns Some () for true, and None for false
-        let inline ofBool b = 
-            if b then Some () else None
+    /// Returns Some () for true, and None for false
+    let inline ofBool b = 
+        if b then Some () else None
 
-        /// Returns Some v if the value can be cast to the given type.
-        let inline cast<'rt> (v: obj) : ('rt option) = 
-            match v with
-            | :? 'rt as v -> Some v
-            | _ -> None
+    /// Returns Some v if the value can be cast to the given type.
+    let inline cast<'rt> (v: obj) : ('rt option) = 
+        match v with
+        | :? 'rt as v -> Some v
+        | _ -> None
 
-    /// Equivalent to the <| operator, but with a more useful priority to separate funs, etc.
-    let inline (--) a b = a b
+/// Equivalent to the <| operator, but with a more useful priority to separate funs, etc.
+let inline (--) a b = a b
 
-    // ordinal startsWith / endsWith for strings
-    type System.String with
-        member this.startsWith part = 
-            this.StartsWith(part, StringComparison.Ordinal)
-        member this.endsWith part = 
-            this.EndsWith(part, StringComparison.Ordinal)
+// ordinal startsWith / endsWith for strings
+type System.String with
+    member this.startsWith part = 
+        this.StartsWith(part, StringComparison.Ordinal)
+    member this.endsWith part = 
+        this.EndsWith(part, StringComparison.Ordinal)
     
-    module List =
-        let inline flatten l = List.collect id l
-        let rec revAndPrepend a l = 
-            match a with
-            | next :: rest -> revAndPrepend rest (next::l) 
-            | [] -> l
+module List =
+    let inline flatten l = List.collect id l
+    let rec revAndPrepend a l = 
+        match a with
+        | next :: rest -> revAndPrepend rest (next::l) 
+        | [] -> l
 
-    module Seq = 
-        let inline flatten s = Seq.collect id s
+module Seq = 
+    let inline flatten s = Seq.collect id s
 
-    module Array = 
-        let inline flatten a = Array.collect id a
+module Array = 
+    let inline flatten a = Array.collect id a
 
-    let inline flip f a b = f b a          
+let inline flip f a b = f b a          
     
-    type Agent<'t> = MailboxProcessor<'t>
-    type 't agent = Agent<'t>
+type Agent<'t> = MailboxProcessor<'t>
+type 't agent = Agent<'t>
 
-    /// Convert a function to an IDisposable.Dispose(), useful for building return values
-    /// that can be used for `use` constructs.
-    let asDisposable f = 
-        { new IDisposable with
-            member this.Dispose() = f() }
+/// Convert a function to an IDisposable.Dispose(), useful for building return values
+/// that can be used for `use` constructs.
+let asDisposable f = 
+    { new IDisposable with
+        member this.Dispose() = f() }
 
-    /// Extend AsyncBuilder with the option bind tasks without using Async.AwaitTask. 
-    /// This is experimental, so only Bind is supported for now.
-    type AsyncBuilder with
-        member this.Bind(t: Task<'t>, f: 't -> Async<'r>) : Async<'r> = 
-            this.Bind(t |> Async.AwaitTask, f)
-        member this.Bind(t: Task, f: unit -> Async<'r>) : Async<'r> = 
-            this.Bind(t |> Async.AwaitTask, f)
+/// Extend AsyncBuilder with the option bind tasks without using Async.AwaitTask. 
+/// This is experimental, so only Bind is supported for now.
+type AsyncBuilder with
+    member this.Bind(t: Task<'t>, f: 't -> Async<'r>) : Async<'r> = 
+        this.Bind(t |> Async.AwaitTask, f)
+    member this.Bind(t: Task, f: unit -> Async<'r>) : Async<'r> = 
+        this.Bind(t |> Async.AwaitTask, f)
 
-        member this.ReturnFrom(t : Task<'r>) : Async<'r> = 
-            t |> Async.AwaitTask
-        member this.ReturnFrom(t : Task) : Async<unit> = 
-            t |> Async.AwaitTask
+    member this.ReturnFrom(t : Task<'r>) : Async<'r> = 
+        t |> Async.AwaitTask
+    member this.ReturnFrom(t : Task) : Async<unit> = 
+        t |> Async.AwaitTask
 
-    /// A predicate & combinator.
-    let (<&>) f g = (fun x -> f x && g x)
+/// A predicate & combinator.
+let (<&>) f g = (fun x -> f x && g x)
 
-    // https://github.com/fsprojects/FSharpx.Extras/blob/master/src/FSharpx.Extras/ComputationExpressions/Monad.fs
-    /// The maybe monad.
-    /// This monad is my own and uses an 'T option. Others generally make their own Maybe<'T> type from Option<'T>.
-    /// The builder approach is from Matthew Podwysocki's excellent Creating Extended Builders series http://codebetter.com/blogs/matthew.podwysocki/archive/2010/01/18/much-ado-about-monads-creating-extended-builders.aspx.
-    type MaybeBuilder() =
-        member __.Return(x) = Some x
-        member __.ReturnFrom(m: 'T option) = m
-        member __.Bind(m, f) = Option.bind f m
-        member __.Zero() = None
-        member __.Combine(m, f) = Option.bind f m
-        member __.Delay(f: unit -> _) = f
-        member __.Run(f) = f()
-        member this.TryWith(m, h) =
-            try this.ReturnFrom(m)
-            with e -> h e
-        member this.TryFinally(m, compensation) =
-            try this.ReturnFrom(m)
-            finally compensation()
-        member this.Using(res:#IDisposable, body) =
-            this.TryFinally(body res, fun () -> match res with null -> () | disp -> disp.Dispose())
-        member this.While(guard, f) =
-            if not (guard()) then Some () else
-            do f() |> ignore
-            this.While(guard, f)
-        member this.For(sequence:seq<_>, body) =
-            this.Using(sequence.GetEnumerator(),
-                fun enum -> this.While(enum.MoveNext, this.Delay(fun () -> body enum.Current)))
+// https://github.com/fsprojects/FSharpx.Extras/blob/master/src/FSharpx.Extras/ComputationExpressions/Monad.fs
+/// The maybe monad.
+/// This monad is my own and uses an 'T option. Others generally make their own Maybe<'T> type from Option<'T>.
+/// The builder approach is from Matthew Podwysocki's excellent Creating Extended Builders series http://codebetter.com/blogs/matthew.podwysocki/archive/2010/01/18/much-ado-about-monads-creating-extended-builders.aspx.
+type MaybeBuilder() =
+    member __.Return(x) = Some x
+    member __.ReturnFrom(m: 'T option) = m
+    member __.Bind(m, f) = Option.bind f m
+    member __.Zero() = None
+    member __.Combine(m, f) = Option.bind f m
+    member __.Delay(f: unit -> _) = f
+    member __.Run(f) = f()
+    member this.TryWith(m, h) =
+        try this.ReturnFrom(m)
+        with e -> h e
+    member this.TryFinally(m, compensation) =
+        try this.ReturnFrom(m)
+        finally compensation()
+    member this.Using(res:#IDisposable, body) =
+        this.TryFinally(body res, fun () -> match res with null -> () | disp -> disp.Dispose())
+    member this.While(guard, f) =
+        if not (guard()) then Some () else
+        do f() |> ignore
+        this.While(guard, f)
+    member this.For(sequence:seq<_>, body) =
+        this.Using(sequence.GetEnumerator(),
+            fun enum -> this.While(enum.MoveNext, this.Delay(fun () -> body enum.Current)))
 
-    let maybe = MaybeBuilder()
+let maybe = MaybeBuilder()
 
 [<assembly:AutoOpen("FunToolbox.Prelude")>]
 do
-    ()    
+()    
 
