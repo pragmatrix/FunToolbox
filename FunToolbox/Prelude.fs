d7579ab855023e13db175054f56f0573c54b2ab8
Michel Bretschneider <mbretsch@questnet.de>
2017-06-23 10:23:46 +0200

    remove all CR(ModuleSuffix) attributes  - no longer needed with F#4.1

--- FunToolbox/Prelude.fs
+++ FunToolbox/Prelude.fs
@@ -9,7 +9,6 @@ type RQAAttribute = RequireQualifiedAccessAttribute
 
 let [<Literal>] ModuleSuffix = CompilationRepresentationFlags.ModuleSuffix
 
-[<CR(ModuleSuffix)>]
 module Option =
     /// Return the value of the option, or elseValue when it is None.
     [<Obsolete("use Option.defaultValue")>]
