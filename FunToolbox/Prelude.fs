f19f94c96eb95251543746e6d57629aead66f223
Armin Sander <armin@replicator.org>
2018-02-12 10:04:36 +0100

    Regex.Match

--- FunToolbox/Prelude.fs
+++ FunToolbox/Prelude.fs
@@ -308,6 +308,17 @@ type Exception with
         | e -> 
             e.Message
 
+module Regex =
+
+    open System.Text.RegularExpressions
+
+    // http://www.fssnip.net/29/title/Regular-expression-active-pattern
+    let (|Match|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success 
+        then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+
 [<assembly:AutoOpen("FunToolbox.Prelude")>]
 do
 ()
