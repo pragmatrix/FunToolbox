5cb1eae92603ebfeab31e2c8ed5db1c0d6ec76c5
Armin Sander <armin@replicator.org>
2018-03-11 07:29:42 +0100

    remove redirects, bump version, add pack.sh to push a nuget package to myget

---
+++ pack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+MSBuild.exe FunToolbox.sln /t:Clean /p:Configuration=Release
+MSBuild.exe FunToolbox.sln /t:FunToolbox /p:Configuration=Release
+mkdir -p tmp
+rm -f tmp/*.nupkg
+(cd FunToolbox && ../.paket/paket pack ../tmp)
+.paket/paket push --url https://www.myget.org/F/pragmatrix --endpoint /api/v2/package --api-key $MYGETAPIKEY tmp/*.nupkg
