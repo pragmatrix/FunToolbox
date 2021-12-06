cfa25b57ae17cca218f08be34d8fe6bfd5aeebc5
Armin Sander <armin@replicator.org>
2021-12-06 16:32:48 +0100

    Update publish script

--- publish.sh
+++ publish.sh
@@ -5,4 +5,4 @@ mkdir -p tmp
 rm -f tmp/*.nupkg
 (cd FunToolbox && dotnet clean -c Release)
 (cd FunToolbox && rm -rf obj bin && dotnet pack -c Release -o ../tmp)
-.paket/paket push --url https://www.myget.org/F/pragmatrix/api/v2/package --api-key $MYGETAPIKEY tmp/*.nupkg
+dotnet paket push --url https://www.myget.org/F/pragmatrix/api/v2/package --api-key $MYGETAPIKEY tmp/*.nupkg
